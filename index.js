621376af0da881c83ebf9c6b0094789cec620b2e
auraz <TaartAndCake789>
2021-12-06 20:12:54 +0100

    Toevoeging delete & edit functie

--- index.js
+++ index.js
@@ -10,28 +10,42 @@ const port = 8080
 
 app.use(bodyParser.json())
 app.use(cors())
-app.listen(process.env.PORT || port, ()=>{
+app.listen(port, ()=>{
     console.log(`application listening on port ${port}`)
 })
 
-app.get("/api/challenges", async (req,res) => {
+app.get("/challenges", async (req,res) => {
     let challenge = await db.getChallenges(mongoClient)
     console.log(challenge)
     res.send(challenge)
 })
 
-app.get("/api/challenge/:id", async (req,res)=>{
+app.get("/challenges/:id", async (req,res)=>{
     let id = new mongoDb.ObjectId(`${req.params.id}`)
     let challenges = await db.getChallenge(mongoClient,id)
     console.log(challenges)
     res.send(challenges)
 })
 
-app.post("/api/insert", async (req,res) =>{
+app.post("/challenges", async (req,res) =>{
     let challenge = req.body
     console.log(challenge)
     await db.addChallenge(mongoClient,challenge)
     res.send(challenge)
+})
+
+app.put("/challenges/:id", async (req,res) =>{
+    let id = new mongoDb.ObjectId(`${req.params.id}`)
+    let newChallenge = req.body
+    await db.updateChallenge(mongoClient,id,newChallenge)
+    let challenge = await db.getChallenge(mongoClient,id)
+    res.send(challenge)
+})
+
+app.delete("/challenges/:id", async (req,res) => {
+    let id = new mongoDb.ObjectId(`${req.params.id}`)
+    await db.deleteChallenge(mongoClient, id)
+    res.send(id)
 })
 
 
